046d9aaa5f36ef9fe76aff7b3341609070336558
jgardner-hub <90541201+jgardner-hub@users.noreply.github.com>
2022-03-16 01:15:07 -0600

    Mission 10, updated database

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -28,6 +28,8 @@ namespace Mission7.Models
         [Required(ErrorMessage = "Please enter a country")]
         public string Country { get; set; }
 
+        [BindNever]
+        public bool PurchaseReceived { get; set; }
 
     }
 }
